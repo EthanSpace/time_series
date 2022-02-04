6bcb9f8fb6185b8ee21e9ad4199e2f07a41350b3
Ethanium <ethan.w1997@outlook.com>
2022-02-03 23:50:19 -0500

    0203 update

--- notes_02b.py
+++ notes_02b.py
@@ -2,6 +2,7 @@
 # Upsampling and interpolation with .resample()
 #------------------------------------------
 
+from matplotlib.image import AxesImage
 import pandas as pd
 from datetime import datetime 
 import numpy as np
@@ -35,7 +36,37 @@ print(unrate.head())
 # Business month end       BM       2017-04-28
 # Business month start     BMS      2017-04-03
 
+# Other frequencies
+# B         business day frequency
+# C         custom business day frequency (experimental)
+# D         calendar day frequency
+# W         weekly frequency
+# M         month end frequency
+# SM        semi-month end frequency (15th and end of month)
+# BM        business month end frequency
+# CBM       custom business month end frequency
+# MS        month start frequency
+# SMS       semi-month start frequency (1st and 15th)
+# BMS       business month start frequency
+# CBMS      custom business month start frequency
+# Q         quarter end frequency
+# BQ        business quarter endfrequency
+# QS        quarter start frequency
+# BQS       business quarter start frequency
+# A         year end frequency
+# BA, BY    business year end frequency
+# AS, YS    year start frequency
+# BAS, BYS  business year start frequency
+# BH        business hour frequency
+# H         hourly frequency
+# T, min    minutely frequency
+# S         secondly frequency
+# L, ms     milliseconds
+# U, us     microseconds
+# N         nanoseconds
 
+
+# asfreq and resample -----------------------------------------------------------------
 print("\nasfreq and resample", "-" * 30)
 # .asfreq() is a selector for the datatime values.
 unrate.asfreq('M').info()
@@ -45,3 +76,61 @@ unrate_r = unrate.resample('M')
 u_equal = unrate.asfreq('MS').equals(unrate_r.asfreq())
 print(u_equal)
 
+
+#  Fill ------------------------------------------------------------------------------
+print("\nFill", "-" * 30)
+unrate.info()
+print(unrate.head(10))
+
+unrate_ffill = unrate.resample('D').ffill().add_suffix('_fill')
+unrate_ffill.info()
+print(unrate_ffill.head(10))
+
+unrate_interp = unrate.resample('D').interpolate().add_suffix('_interp')
+unrate_interp.info()
+print(unrate_interp.head(10))
+
+
+# Concatenating two DataFrames ------------------------------------------------------
+print("\nConcatenating two DataFrames", "-" * 30)
+
+df1 = pd.DataFrame([1, 2, 3], columns = ['df1'])
+df2 = pd.DataFrame([4, 5, 6], columns = ['df2'])
+df3 = pd.concat([df1, df2])
+print(df3.head(10))
+
+# axis = 1: concatenate horizontally
+print('\nwith axis = 1')
+df4 = pd.concat([df1, df2], axis = 1)
+print(df4.head(10))
+
+df5 = pd.concat([unrate_ffill, unrate_interp], axis = 1)
+print(df5.head(10))
+df5.loc['2001':'2010'].plot()
+#plt.show()
+
+
+# Downsampling and aggregation methods -----------------------------------------------
+print("\nDownsampling and aggregation methods", "-" * 30)
+nio = pd.read_csv('NIO.csv', parse_dates = ['Date'], index_col = 'Date')
+nio.info()
+
+# https://blog.csdn.net/qifeidemumu/article/details/88764211
+print("\n price DataFrame")
+price = pd.DataFrame(nio.Close).resample('D').asfreq().dropna()
+print(type(price))
+price.info()
+
+price_mean = price.resample('M').mean()
+print('\n', price_mean.head())
+price_median = price.resample('M').median()
+print('\n', price_median.head())
+
+# .agg() takes a list of / a np function name. e.g. np.min np.sum
+price_agg = price.resample('M').agg(['mean', 'std']).head()
+print('\n', price_agg.head())
+
+ax_p = price.plot()
+monthly = price.resample('M').mean()
+monthly.add_suffix('_monthly').plot(ax = ax_p)
+plt.show()
